8dd040ee14be22273aee4eeeea7464fe4cc22fca
Muhammad-Bilal321 <128062466+Muhammad-Bilal321@users.noreply.github.com>
2023-03-31 02:21:25 +0500

    Add files via upload

---
+++ Javascript1/app.js
@@ -0,0 +1,78 @@
+// Alert
+
+// alert("1st JavaScript Class");
+
+// Variables for Strings
+var carName = "BMW";
+document.write(carName);
+console.log(carName);
+
+// Variables for Numbers
+// Addition
+console.log("Addition");
+var a1 = 2;
+var b1 = 2;
+var c1 = a1+b1;
+console.log(c1);
+// Subtraction
+console.log("Subtraction");
+var a1 = 4;
+var b1 = 2;
+var c1 = a1-b1;
+console.log(c1);
+// Multiplication
+console.log("Multiplication");
+var a1 = 2;
+var b1 = 2;
+var c1 = a1*b1;
+console.log(c1);
+// Division
+console.log("Division");
+var a1 = 4;
+var b1 = 2;
+var c1 = a1/b1;
+console.log(c1);
+
+// Math Expressions: unfamiliar operators
+// increment
+console.log("increment");
+var a1 = 2;
+var b1 = 2;
+var c1 = ++a1 + b1;
+console.log(c1);
+// Decrement
+console.log("Decrement");
+var a1 = 2;
+var b1 = 2;
+var c1 = --a1 + b1;
+console.log(c1);
+// Math Expressions: eliminating ambiguity
+console.log("eliminating ambiguity");
+var a1 = 2;
+var b1 = 2;
+var c1 = a1 + b1 * a1 / b1;
+console.log(c1);
+// Concatenating text strings
+console.log("Concatenating text strings");
+var Name = "Bilal";
+var City = "Karachi";
+console.log(Name + (" ") + ("From") + (" ") + City  );
+
+// Prompts
+// console.log("Prompts");
+// var Name1 = prompt("Your Name","Arsalan");
+// var City1 = prompt("Your City", "Karachi");
+
+// console.log(Name1 + (" ") + ("From") + (" ") + City1);
+
+// if statements & Comparison operators & if...else
+var Marks= prompt("Enter Marks");
+if (Marks >= 70) {
+    console.log("Passed")
+    
+} else {
+    console.log("Failed")
+}
+
+
+
